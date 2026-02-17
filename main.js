7296b07500f443809d752ea63c04526b076347a6
evycham <ygruzevich8484@gmail.com>
2026-02-17 20:36:45 +0100

    can create tasks and they will be shown

--- main.js
+++ main.js
@@ -3,24 +3,29 @@
 
 const btnOpen = document.getElementById("btnOpen");
 const btnClose = document.getElementById("btnClose");
-const btnCreate = document.getElementById("btnCreate");
+
 const dialog = document.getElementById("dialog");
 const form = document.getElementById("form");
+
 const titelInput = document.getElementById("titel--input");
 const descriptionInput = document.getElementById("description--input");
 const dateInput = document.getElementById("date--input");
+
 const tasksList = document.getElementById("tasks--list");
 const errorMsg = document.getElementById("error");
+const template = document.getElementById("tasks--template");
 
 
 let tasksArray = [];
 
 // Dialog-open
 btnOpen.addEventListener("click", function() {
+    clearInput();
     dialog.showModal();
 });
 // Dialog-close
 btnClose.addEventListener("click", function() {
+    clearInput();
     dialog.close();
 });
 
@@ -30,48 +35,56 @@ function createTask(e) {
     e.preventDefault();
 
     const dateToday = new Date();
+    dateToday.setHours(0, 0, 0, 0);
 
     const name = titelInput.value;
     const description = descriptionInput.value;
     const date = new Date(dateInput.value);
 
-    let day = date.getDay();
-
-    if(dateToday <= date){
+    if(dateToday > date){
         showError();
         return;
     }
 
     let task = new Task(name, description, date);
     tasksArray.push(task);
-    updateList();
+
+    task.addTask();
+    hideError();
 }
 
 class Task{
-    constructor(_name, _description, _date) {
-        this.name = _name;
+    constructor(_title, _description, _date) {
+        this.title = _title;
         this.description = _description;
         this.date = _date;
     }
-}
 
-function updateList(){
-    let allTasks = Array.from(tasksList.getElementsByTagName("li"));
+    addTask() {
+        const clone = template.content.cloneNode(true);
+        const title = clone.querySelector(".task--title");
+        const description = clone.querySelector(".task--description");
+        const date = clone.querySelector(".task--deadline");
 
-    allTasks.forEach(task => {
-        task.remove();
-    });
+        title.textContent = this.title;
+        description.textContent = this.description;
+        date.textContent = this.date;
 
-    for(let obj of tasksArray){
-
+        tasksList.appendChild(clone);
     }
 }
 
 function showError(){
-    errorMsg.classList.add("errorOn");
+    errorMsg.classList.add("error--On");
 }
 
 function hideError(){
-    errorMsg.classList.remove("errorOn");
+    errorMsg.classList.remove("error--On");
 }
 
+function clearInput(){
+    titelInput.value = null;
+    descriptionInput.value = null;
+    dateInput.value = null;
+    dialog.close();
+}
